a9d987aa1050a81a88cf24a9054084330a42efb3
Fredrik Sørensen <fredrik@vol11.com>
2019-11-04 16:50:31 +0100

    Update dependencies

--- src/core/intervals/day.ts
+++ src/core/intervals/day.ts
@@ -2,3 +2,7 @@ import { interval } from './interval'
 import { DAY } from '../constants'
 
 export const day = interval((ts: number) => ts, DAY)
+
+const newDay = day(Date.now())
+
+console.log(newDay)
